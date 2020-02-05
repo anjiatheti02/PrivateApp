63374eea1336be29fe04bc69e54b2b69b443421a
Anjaneyulu Atteti <anjaneyulu.atteti@itcinfotech.com>
2020-02-05 15:22:53 +0530

    Initial Commit

---
+++ DynamicUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicUI
+//
+//  Created by Admin on 05/02/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
